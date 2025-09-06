f9343d1fa595528ee30dd2a306f4b5e6820b72df
PetrakovVladyslav <petrakov.14ca114@gmail.com>
2025-09-06 10:29:28 +0300

    Update userconfig.js

--- userconfig.js
+++ userconfig.js
@@ -322,8 +322,8 @@ const default_config = {
         }
     ]
 };
-// const CONFIG = new Config(saved_config ?? default_config);
-const CONFIG = new Config(default_config);
+ const CONFIG = new Config(saved_config ?? default_config);
+//const CONFIG = new Config(default_config);
 
 (function() {
   var css = document.createElement('link');
